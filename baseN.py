f9a416278fa1ccd7b1fc9a4e33221302370ec617
Yasuaki Uechi <y@uechi.io>
2019-06-03 17:07:03 +0900

    reformat

--- baseN.py
+++ baseN.py
@@ -8,9 +8,7 @@ import argparse
 def encode(text, encodeMap, encodingBit):
     binary = ''.join([format(x, '08b') for x in text.encode('utf8')])
     binary += (encodingBit - len(binary) % encodingBit) * '0'
-    splittedArray = [
-        binary[x:x + encodingBit] for x in range(0, len(binary), encodingBit)
-    ]
+    splittedArray = [binary[x:x + encodingBit] for x in range(0, len(binary), encodingBit)]
     encodedString = ''.join([encodeMap[int(x, 2)] for x in splittedArray])
     encodedString += (2 - len(encodedString) % 2) * '='
     return encodedString
@@ -31,18 +29,12 @@ if __name__ == '__main__':
     parser.add_argument('text', help='source text')
     parser.add_argument('--map', type=argparse.FileType(), help='map file')
     parser.add_argument(
-        '--decode',
-        action='store_const',
-        const=decode,
-        default=encode,
-        dest='function',
-        help='decode mode')
+        '--decode', action='store_const', const=decode, default=encode, dest='function', help='decode mode')
     args = parser.parse_args()
 
     encodeMap = list(args.map.read())
     encodingBit = math.floor(math.log2(len(encodeMap)))
-    print('Base' + str(2**encodingBit) + ' (mapsize: ' + str(len(encodeMap)) +
-          ')')
+    print('Base' + str(2**encodingBit) + ' (mapsize: ' + str(len(encodeMap)) + ')')
 
     source = args.text
     print('source: ' + source)
